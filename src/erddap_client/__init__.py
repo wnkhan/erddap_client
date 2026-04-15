998443585fbbad4229b5e7837d7f2b2c3d8fd9a5
William Khan <wnkhan32@gmail.com>
2026-04-15 16:24:54 -0500

    Added erddap wrapper for ingestion of glider datasets.

--- src/erddap_client/__init__.py
+++ src/erddap_client/__init__.py
@@ -1,5 +1,6 @@
 """Utilities for exploring ERDDAP servers and datasets."""
 
 from .mapping import make_map_axes
+from .erddap_wrapper import GliderIngestor
 
-__all__ = ["make_map_axes"]
+__all__ = ["make_map_axes", "GliderIngestor"]
---
+++ src/erddap_client/erddap_wrapper.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from erddapy import ERDDAP, servers
+
+GLIDER_SERVER_NAME = "ngdac"
+GLIDER_URL = str(servers[GLIDER_SERVER_NAME].url)
+
+GLIDER_SERVER_INFO = {
+    "server": GLIDER_URL,
+    "protocol": "tabledap",
+    "response": "csv",
+}
+
+
+class GliderIngestor:
+
+    def __init__(self):
+        self.e = ERDDAP(**GLIDER_SERVER_INFO)
+
+    def dataset_search(
+        self,
+        search_for='"sound speed" salinity temp',
+        *,
+        min_lat=None,
+        max_lat=None,
+        min_lon=None,
+        max_lon=None,
+        protocol=None,
+        response="csv",
+        **search_filters,
+    ):
+        """Search the glider ERDDAP catalog and return the results as a DataFrame."""
+        bounds = {
+            "min_lat": min_lat,
+            "max_lat": max_lat,
+            "min_lon": min_lon,
+            "max_lon": max_lon,
+        }
+        search_params = {
+            key: value
+            for key, value in bounds.items()
+            if value is not None
+        }
+        search_params.update(search_filters)
+
+        url = self.e.get_search_url(
+            search_for=search_for,
+            response=response,
+            protocol=protocol,
+            **search_params,
+        )
+        return pd.read_csv(url).drop_duplicates()
+
+    def get_dataset(self, dataset_id):
+        self.e.dataset_id = dataset_id
+        return pd.read_csv(self.e.get_download_url())
